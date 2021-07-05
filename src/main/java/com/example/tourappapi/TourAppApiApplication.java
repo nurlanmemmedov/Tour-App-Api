453a8be418a75a2006f1f7df7b39f99ca5b12708
= <nurlanam@code.edu.az>
2021-07-06 00:45:12 +0400

    Add project initial structure

---
+++ src/main/java/com/example/tourappapi/TourAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tourappapi/TourAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
